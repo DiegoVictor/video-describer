96687790f9b0a3c2cbfb2b702ccf102fe71b7016
Diego Victor <diegovictorgonzaga@gmail.com>
2023-09-14 10:28:32 -0300

    Register routes and cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import { prompts } from './routes/prompts';
+import { uploads } from './routes/uploads';
+import { transcriptions } from './routes/transcriptions';
 import { env } from './env';
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: '*',
+});
+
+app.register(prompts);
+app.register(uploads);
+app.register(transcriptions);
 
 app
   .listen({
