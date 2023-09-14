87fd7da6e6a7316c5c77ae708d2437e6f9b774e8
Diego Victor <diegovictorgonzaga@gmail.com>
2023-09-14 10:22:49 -0300

    Update environment variables validation

--- src/env.ts
+++ src/env.ts
@@ -1,23 +1,26 @@
-import "dotenv/config";
-import { z } from "zod";
+import 'dotenv/config';
+import { z } from 'zod';
 
 const schema = z.object({
   DATABASE_URL: z.string({
-    required_error: "Missing DATABASE_URL in environment variable",
+    required_error: 'Missing DATABASE_URL in environment variable',
   }),
-  DATABASE_CLIENT: z.enum(["sqlite"]),
+  DATABASE_CLIENT: z.enum(['sqlite']),
   PORT: z.coerce
     .number({
-      invalid_type_error: "PORT must be a number",
+      invalid_type_error: 'PORT must be a number',
     })
     .default(3000),
   NODE_ENV: z
-    .enum(["dev", "test"], {
+    .enum(['dev', 'test'], {
       invalid_type_error:
-        "NODE_ENV must one of the following: development or test",
-      required_error: "Missing NODE_ENV in environment variables",
+        'NODE_ENV must one of the following: development or test',
+      required_error: 'Missing NODE_ENV in environment variables',
     })
-    .default("dev"),
+    .default('dev'),
+  OPENAI_KEY: z.string({
+    required_error: 'Missing Open AI API Key',
+  }),
 });
 
 export const env = schema.parse(process.env);
