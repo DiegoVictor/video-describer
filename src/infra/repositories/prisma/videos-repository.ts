9cfddcd3b437eabbc32be5a447c6a35c91a2043c
Diego Victor <diegovictorgonzaga@gmail.com>
2023-09-25 00:08:49 -0300

    Move repositories to infra folder

---
+++ src/infra/repositories/prisma/videos-repository.ts
@@ -0,0 +1,33 @@
+import { prisma } from './prisma';
+import { IVideosRepository, IVideo } from '../../../contracts/videos';
+
+export class PrismaVideosRepository implements IVideosRepository {
+  public async create(data: IVideo): Promise<IVideo> {
+    const video = await prisma.video.create({
+      data,
+    });
+
+    return video;
+  }
+
+  public async findOneById(id: string): Promise<IVideo | null> {
+    const video = await prisma.video.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return video;
+  }
+
+  public async updateById(id: string, data: Partial<IVideo>): Promise<IVideo> {
+    const video = await prisma.video.update({
+      where: {
+        id,
+      },
+      data,
+    });
+
+    return video;
+  }
+}
