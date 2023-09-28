2870c7868147033fb208907f583c463b52f2c063
Diego Victor <diegovictorgonzaga@gmail.com>
2023-09-28 18:23:37 -0300

    Test get prompts

---
+++ tests/unit/get-prompts.spec.ts
@@ -0,0 +1,18 @@
+import { factory } from '../utils/factory';
+import { IPrompt } from '../../src/contracts/prompts';
+import { GetPromptsUseCase } from '../../src/use-cases/get-prompts';
+
+describe('GetPromptsUseCase', () => {
+  it('should be able to get prompts', async () => {
+    const prompts: IPrompt[] = await factory.attrsMany('Prompt', 3);
+    const repository = {
+      getAll: jest.fn().mockResolvedValueOnce(prompts),
+    };
+    const getPromptsUseCase = new GetPromptsUseCase(repository);
+
+    const response = await getPromptsUseCase.execute();
+
+    expect(repository.getAll).toHaveBeenCalled();
+    expect(response).toStrictEqual(prompts);
+  });
+});
