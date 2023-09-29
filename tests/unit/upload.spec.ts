c90cae6eb2ff7b3874b449082799070654288c24
Diego Victor <diegovictorgonzaga@gmail.com>
2023-09-29 09:34:03 -0300

    Test upload use case

---
+++ tests/unit/upload.spec.ts
@@ -0,0 +1,78 @@
+import { faker } from '@faker-js/faker';
+import { Readable } from 'stream';
+import { MultipartFile } from '@fastify/multipart';
+import { factory } from '../utils/factory';
+import { UploadUseCase } from '../../src/use-cases/upload';
+import { IVideo } from '../../src/contracts/videos';
+
+const mockPromisify = jest.fn();
+jest.mock('node:util', () => {
+  return {
+    promisify: () => mockPromisify(),
+  };
+});
+
+jest.mock('node:fs', () => {
+  return {
+    createWriteStream: jest.fn(),
+  };
+});
+
+describe('UploadUseCase', () => {
+  it('should be able to upload file', async () => {
+    const video: IVideo = await factory.attrs('Video');
+    const repository = {
+      create: jest.fn().mockResolvedValueOnce(video),
+      findOneById: jest.fn(),
+      updateById: jest.fn(),
+    };
+    const uploadUseCase = new UploadUseCase(repository);
+
+    mockPromisify.mockReturnValueOnce(async () => {});
+
+    const data = {
+      filename: faker.helpers.slugify(faker.lorem.words(3)).concat('.mp3'),
+      file: new Readable(),
+    } as MultipartFile;
+    const response = await uploadUseCase.execute({ data });
+
+    expect(repository.create).toHaveBeenCalledWith({
+      name: data.filename,
+      path: expect.any(String),
+    });
+    expect(response).toStrictEqual(video);
+  });
+
+  it('should not be able to upload a file that is not a MP3', async () => {
+    const video: IVideo = await factory.attrs('Video');
+    const repository = {
+      create: jest.fn().mockResolvedValueOnce(video),
+      findOneById: jest.fn(),
+      updateById: jest.fn(),
+    };
+    const uploadUseCase = new UploadUseCase(repository);
+
+    mockPromisify.mockReturnValueOnce(async () => {});
+
+    const data = {
+      filename: faker.helpers.slugify(faker.lorem.words(3)).concat('.mp4'),
+      file: new Readable(),
+    } as MultipartFile;
+    await expect(async () => uploadUseCase.execute({ data })).rejects.toThrow();
+  });
+
+  it('should not be able to upload a file that is not a MP3', async () => {
+    const video: IVideo = await factory.attrs('Video');
+    const repository = {
+      create: jest.fn().mockResolvedValueOnce(video),
+      findOneById: jest.fn(),
+      updateById: jest.fn(),
+    };
+    const uploadUseCase = new UploadUseCase(repository);
+
+    mockPromisify.mockReturnValueOnce(async () => {});
+
+    const data = undefined;
+    await expect(async () => uploadUseCase.execute({ data })).rejects.toThrow();
+  });
+});
