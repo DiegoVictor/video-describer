c4a60ca32dcecbdf1ac464cbac92777bf403097e
Diego Victor <diegovictorgonzaga@gmail.com>
2023-10-02 08:56:14 -0300

    Test video description creation

---
+++ tests/unit/describe-video.spec.ts
@@ -0,0 +1,104 @@
+import { faker } from '@faker-js/faker';
+import { factory } from '../utils/factory';
+import { DescribeVideoUseCase } from '../../src/use-cases/describe-video';
+import { IVideo } from '../../src/contracts/videos';
+
+describe('DescribeVideoUseCase', () => {
+  it('should be able to describe a video', async () => {
+    const video = await factory.attrs<Required<IVideo>>('Video');
+    const repository = {
+      create: jest.fn(),
+      findOneById: jest.fn(),
+      updateById: jest.fn(),
+    };
+    const service = {
+      createAudioTranscription: jest.fn(),
+      createChatCompletion: jest.fn(),
+    };
+
+    repository.findOneById.mockResolvedValueOnce(video);
+
+    const describeVideoUseCase = new DescribeVideoUseCase(repository, service);
+
+    const stream = {};
+    service.createChatCompletion.mockResolvedValueOnce(stream);
+
+    const prompt = faker.lorem.words(10);
+    const temperature = 0.5;
+    const response = await describeVideoUseCase.execute({
+      id: video.id,
+      prompt,
+      temperature,
+    });
+
+    expect(repository.findOneById).toHaveBeenCalledWith(video.id);
+    expect(service.createChatCompletion).toHaveBeenCalledWith(
+      temperature,
+      prompt
+    );
+    expect(response).toStrictEqual(stream);
+  });
+
+  it('should not be able to describe a video that does not have a transcription', async () => {
+    const video = await factory.attrs<Required<IVideo>>('Video', {
+      transcription: undefined,
+    });
+    const repository = {
+      create: jest.fn(),
+      findOneById: jest.fn(),
+      updateById: jest.fn(),
+    };
+    const service = {
+      createAudioTranscription: jest.fn(),
+      createChatCompletion: jest.fn(),
+    };
+
+    repository.findOneById.mockResolvedValueOnce(video);
+
+    const describeVideoUseCase = new DescribeVideoUseCase(repository, service);
+
+    const stream = {};
+    service.createChatCompletion.mockResolvedValueOnce(stream);
+
+    const prompt = faker.lorem.words(10);
+    const temperature = 0.5;
+    await expect(async () =>
+      describeVideoUseCase.execute({
+        id: video.id,
+        prompt,
+        temperature,
+      })
+    ).rejects.toThrow();
+
+    expect(repository.findOneById).toHaveBeenCalledWith(video.id);
+  });
+
+  it('should not be able to describe a video that not exists', async () => {
+    const video = await factory.attrs<Required<IVideo>>('Video');
+    const repository = {
+      create: jest.fn(),
+      findOneById: jest.fn(),
+      updateById: jest.fn(),
+    };
+    const service = {
+      createAudioTranscription: jest.fn(),
+      createChatCompletion: jest.fn(),
+    };
+
+    repository.findOneById.mockResolvedValueOnce(undefined);
+
+    const describeVideoUseCase = new DescribeVideoUseCase(repository, service);
+
+    const prompt = faker.lorem.words(10);
+    const temperature = 0.5;
+    await expect(async () =>
+      describeVideoUseCase.execute({
+        id: video.id,
+        prompt,
+        temperature,
+      })
+    ).rejects.toThrow();
+
+    expect(repository.findOneById).toHaveBeenCalledWith(video.id);
+  });
+});
