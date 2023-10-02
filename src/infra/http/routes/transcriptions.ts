240c34e7c596c36be1d7924c58adcd612e99b00d
Diego Victor <diegovictorgonzaga@gmail.com>
2023-10-02 08:56:31 -0300

    Create route for video description use case

--- src/infra/http/routes/transcriptions.ts
+++ src/infra/http/routes/transcriptions.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
+import { streamToResponse } from 'ai';
 import { makeCreateTranscriptionUseCase } from '../../../use-cases/factory/make-create-transcription-use-case';
+import { makeDescribeVideoUseCase } from '../../../use-cases/factory/make-describe-video-use-case';
+
 export async function transcriptions(app: FastifyInstance) {
   app.post('/videos/:id/transcription', async request => {
     const { id } = z
@@ -24,4 +27,32 @@ export async function transcriptions(app: FastifyInstance) {
     return transcription;
   });
 
+  app.post('/videos/:id/generate', async (request, reply) => {
+    const { id } = z
+      .object({
+        id: z.string().uuid(),
+      })
+      .parse(request.params);
+
+    const { prompt, temperature } = z
+      .object({
+        prompt: z.string(),
+        temperature: z.number().min(0).max(1).default(0.5),
+      })
+      .parse(request.body);
+
+    const describeVideoUseCase = makeDescribeVideoUseCase();
+    const stream = await describeVideoUseCase.execute({
+      id,
+      prompt,
+      temperature,
+    });
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      },
+    });
+  });
 }
