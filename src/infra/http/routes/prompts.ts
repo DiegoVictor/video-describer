2e17cd290c522e2ad038b1e34a8ab13dc6cbd94c
Diego Victor <diegovictorgonzaga@gmail.com>
2023-10-06 10:42:54 -0300

    Update get prompts to use FEH too

--- src/infra/http/routes/prompts.ts
+++ src/infra/http/routes/prompts.ts
@@ -1,11 +1,12 @@
 import { FastifyInstance } from 'fastify';
 import { makeGetPromptsUseCase } from '../../../use-cases/factory/make-get-prompts-use-case';
+import { HttpResponse } from '../../helpers/http-response';
 
 export async function prompts(app: FastifyInstance) {
-  app.get('/prompts', async () => {
+  app.get('/prompts', async (_, reply) => {
     const getPrompts = makeGetPromptsUseCase();
-    const prompts = await getPrompts.execute();
+    const result = await getPrompts.execute();
 
-    return prompts;
+    return HttpResponse.parse(result, reply);
   });
 }
--- src/use-cases/get-prompts.ts
+++ src/use-cases/get-prompts.ts
@@ -1,11 +1,12 @@
-import { IPromptsRepository } from '../contracts/prompts';
+import { IPrompt, IPromptsRepository } from '../contracts/prompts';
+import { IEither, success } from '../contracts/either';
 
 export class GetPromptsUseCase {
   constructor(private promptsRepository: IPromptsRepository) {}
 
-  public async execute() {
+  public async execute(): Promise<IEither<IPrompt[]>> {
     const prompts = await this.promptsRepository.getAll();
 
-    return prompts;
+    return success(prompts);
   }
 }
